88443e6df170cd555e63a9cad7d3932dbad4b8a3
CNNunez <nnunez@ic-itcr.ac.cr>
2021-09-29 21:50:17 -0600

    Fix access error and add main function and plot results

--- DatosRemotos.py
+++ DatosRemotos.py
@@ -1,9 +1,6 @@
-#from timeit import default_timer as timer
+from timeit import default_timer as timer
 import matplotlib.pyplot as plt
 import poplib
-import os
-import string
-import logging
 
 
 
@@ -13,8 +10,8 @@ import logging
         A continuacion se presentan las funciones creadas para el desarrollo
         del proyecto.
 
-        Utiliza pop3 para poder conectar con el server del corre. Es necesario
-        instalar esto: pip install Aspose.Email-for-Python-via-NET
+        Utiliza poplib para poder conectar con el server del corre. Es necesario
+        instalar esto: pip install pycopy-poplib
 """
 
 
@@ -24,8 +21,8 @@ import logging
 def graficar(lista_n, lista_y, titulo, color):
     plt.plot(lista_n,lista_y,'-',linewidth=3,color=color)
     plt.grid()
-    plt.xlabel('Numero de muestra')
-    plt.ylabel('Tiempo')
+    plt.xlabel('Numero de muestra (n)')
+    plt.ylabel('Tiempo (s)')
     plt.title(titulo)
     plt.show()
 
@@ -33,71 +30,64 @@ def graficar(lista_n, lista_y, titulo, color):
 """
     Leer correos del servidor
 """
-def ReadEmail():
-    print('START')
+def ReadEmail(user, pwd, ht, pp):
+    username = user
+    passwd = pwd
+    host = ht
+    port = pp
 
-    user_name = 'testeremail.001.2021@gmail.com'
-    passwd = '001.2021'
-    pop3_server_domain = 'pop.gmail.com'
-    pop3_server_port = '995'
+    time = 0
     
     # Connect to pop3 email server.
-    print('   start connect')
-    mail_box = poplib.POP3(pop3_server_domain, pop3_server_port) 
-    mail_box.user(user_name) 
-    mail_box.pass_(passwd)
+    try:
+        start = timer()
+        print('   start connect')
+        server  = poplib.POP3_SSL(host, port) 
+        server .set_debuglevel(1)
+        server .user(username) 
+        server .pass_(passwd)
 
-    # Get number of existing emails.
-    number_of_messages = len(mail_box.list()[1])
-    # Loop in the all emails.
-    for i in range(number_of_messages):
-        print('   in for')
-        # Get one email.
-        for msg in mail_box.retr(i+1)[1]:
-            # Get the email from address. 
-            msg_r = msg.get('From')
-            print('This message is: ' + msg_r)
-    mail_box.quit()
-    print('END')
-
-
-
-
-#ReadEmail()
-
-
-def prueba():
-    SERVER = "pop.gmail.com"
-    USER  = 'testeremail.001.2021@gmail.com'
-    PASSWORD = '001.2021'
-
-    # connect to server
-    logging.debug('connecting to ' + SERVER)
-    server = poplib.POP3_SSL(SERVER)
-    #server = poplib.POP3(SERVER)
-
-    # login
-    logging.debug('logging in')
-    server.user(USER)
-    server.pass_(PASSWORD)
-
-    # list items on server
-    logging.debug('listing emails')
-    # Get number of existing emails.
-    number_of_messages = len(server.list()[1])
-    # Loop in the all emails.
-    for i in range(number_of_messages):
-        print('   in for')
-        # Get one email.
-        for msg in server.retr(i+1)[1]:
-            # Get the email from address. 
-            msg_r = msg.get('From')
-            print('This message is: ' + msg_r)
-    server.quit()
-    print('END')
+        # Loop through list of emails
+        for i in range(len(server .list()[1])):
+            print('   in for')
+            
+            # Get one email.
+            for msg in server .retr(i+1)[1]:
+                # Print message
+                print(msg)
+        server .quit()
+        end = timer()
+        time = (end-start)*1000000000
+    except Exception as error:
+        print (" -> ERROR: ", str(error))
+    return time
 
 
 
+def mail():
+    # Variables
+    cantidad_Test = 10
+    result_time = []
+    n = []
 
-prueba()
+    # Informacion de cuenta de prueba
+    username = 'testeremail.001.2021@gmail.com'
+    password = '001.2021'
+    hostGmail = 'pop.gmail.com'
+    portGmail = '995'
+    
+    # Realizar las pruebas y guardar los resultados
+    print('START Test')
+    for i in range(cantidad_Test):
+        print(i)
+        time = ReadEmail(username,password,hostGmail,portGmail)
+        result_time.append(time)
+        n.append(i)
+    print('END Test')
+    print(n)
+    print(result_time)
+    # Graficar resultados obtenidos
+    graficar(n, result_time, 'Prueba', 'r')
 
+
+mail()
