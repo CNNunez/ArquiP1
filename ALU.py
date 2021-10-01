a8f3fb67876b4956b6d15922d0eadd907c32a00c
David Salazar <50851287+Kbaio@users.noreply.github.com>
2021-09-30 21:49:39 -0600

    Add files via upload

---
+++ ALU.py
@@ -0,0 +1,63 @@
+from timeit import default_timer as timer
+import matplotlib.pyplot as plt
+import math
+import random
+
+#Global
+num = 1000;
+
+def graficar(lista_n, lista_y, titulo, color):
+    plt.plot(lista_n,lista_y,'-',linewidth=3,color=color)
+    plt.grid()
+    plt.xlabel('Numero de muestra (n)')
+    plt.ylabel('Tiempo (ms)')
+    plt.title(titulo)
+    plt.show()
+
+def Operaciones( a, b):
+    contador = 0;
+    for i in range(num):
+        if(contador == 0):
+            a[i] = math.sqrt(a[i]);
+            contador+=1;
+        elif(contador == 1):
+            a[i] *= b[i];
+            contador+=1;
+        elif(contador == 2):
+            a[i] = math.pow(a[i],b[i]);
+            contador+=1;
+        elif(contador == 3):
+            a[i] /= b[i];
+            contador+=1;
+        elif(contador == 4):
+            a[i] = math.log(a[i]+b[i]) ;
+            contador+=1;
+        else:
+            a[i]+=b[i];
+            contador = 0;
+
+def llenarLista(lista):
+    for i in range(num):
+        lista.append(random.randint(1,100));
+
+def main():
+    repeticiones = 100;
+    tiempos = [];
+    reps = [];
+
+    for i in range(repeticiones):
+        lista1 = [];
+        lista2 = [];
+        llenarLista(lista1);
+        llenarLista(lista2);
+
+        start = timer();
+        Operaciones(lista1,lista2);
+        end = timer();
+        time = (end-start);
+        tiempos.append(time);
+        reps.append(i);
+    
+    graficar(reps,tiempos,"Prueba A",'r');
+
+main();
