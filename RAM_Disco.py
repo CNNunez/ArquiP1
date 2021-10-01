6b1d0333e5bec10943e7c0ad3588e3d24ea4ad79
AndresMasis <andres-masis-rojas@outlook.com>
2021-09-30 21:27:39 -0600

    Done until 1mb

--- RAM_Disco.py
+++ RAM_Disco.py
@@ -1,42 +1,57 @@
 import timeit
 import os
-
-
-# This part measures the time the RAM needs to load data from the hard drive
 from matplotlib import pyplot as plt
 
 
-def from_drive_to_ram_mini():
-    f = open("MiniTestRead.txt", "r")
-    f.read()
+ultra_mini = 17  # 17 bytes
+mini = 215  # 215 bytes
+small = 786  # 786 bytes
+kilobyte = 1024  # 1 kb
+medium_small = 86016  # 86kb
+medium = 293888  # 287kb
+megabyte = 1048576  # 1mb
+
+
+# This part measures the time the RAM needs to load data from the hard drive
+
+
+def from_drive_to_ram():
+    f = open("Test.txt", "r")
+    x = f.read()
     f.close()
 
 
-def print_drive_to_ram_mini():
-    size = os.path.getsize("MiniTestRead.txt")
-    print("Tiempo para cargar un mini archivo de " + str(size) + " bytes")
-    print("Tiempo transcurrido: " + format(timeit.Timer(from_drive_to_ram_mini).timeit(number=1), '.8f') + " segundos")
+def print_drive_to_ram(size):
+    time = 1000000 * timeit.timeit(lambda: from_drive_to_ram(), number=1)
+    time = "{:f}".format(time)
+
+    print("Tiempo para cargar un archivo de " + str(size) + " bytes. Pasar data de disco duro a RAM")
+    print("Tiempo transcurrido: " + time + " microsegundos")
+
+    return float(time)
 
 
 # This part measures the time the RAM needs to save data permanently into the hard drive
 
 
-def ram_writes_in_drive_mini():
-    f = open("TestWrite.txt", "w")
-    f.write("123456789")
+def ram_writes_in_drive(my_string):
+    f = open("Test.txt", "w")
+    f.write(my_string)
     f.close()
 
 
-def print_ram_writes_mini():
-    size = os.path.getsize("TestWrite.txt")
-    print("Tiempo para pasar 9 bytes de RAM a un archivo a disco duro para guardarlo permanentemente")
+def print_ram_writes(size):
+    print("Tiempo para escribir " + str(size) + " bytes en un archivo. Escribir data de RAM a disco duro.")
 
-    time = timeit.timeit(ram_writes_in_drive_mini, number=1)
+    my_string = ""
+    for i in range(0, size):
+        my_string += "1"
+
+    time = 1000000*timeit.timeit(lambda: ram_writes_in_drive(my_string), number=1)
     time = "{:f}".format(time)
-    print("Tiempo transcurrido: " + time + " segundos")
+    print("Tiempo transcurrido: " + time + " microsegundos")
 
-    return time
-
+    return float(time)
 
 
 def graficar(lista_n, lista_y, titulo, color):
@@ -48,13 +63,38 @@ def graficar(lista_n, lista_y, titulo, color):
     plt.show()
 
 
-result_time = []
-n = []
+def draw_info(size):
+    result_time = []
+    n = []
+    # Write data. RAM to drive
 
-for i in range(1, 4):
-    result_time.append(print_ram_writes_mini())
-    n.append(i)
+    for i in range(1, 7):
+        print(i)
+        result_time.append(print_ram_writes(size))
+        print("")
+        n.append(i)
+
+    graficar(n, result_time, 'Escribir ' + str(size), 'r')
+    print("\n")
+
+    # Read data. RAM to drive
+    result_time2 = []
+    n2 = []
+
+    for i in range(1, 7):
+        print(i)
+        result_time2.append(print_drive_to_ram(size))
+        print("")
+        n2.append(i)
+
+    print("\n\n")
+    graficar(n2, result_time2, 'Leer ' + str(size), 'r')
 
 
-graficar(n, result_time, 'Escribir mini', 'r')
-
+draw_info(ultra_mini)
+draw_info(mini)
+draw_info(small)
+draw_info(kilobyte)
+draw_info(medium_small)
+draw_info(medium)
+draw_info(megabyte)
